ebb71f5421a659739aadfeb1b80fb68bef757fa2
Philip Jensen <me@iensenfirippu.dk>
2014-04-18 06:33:37 +0200

    -

--- themes/default/admin/index.php
+++ themes/default/admin/index.php
@@ -27,6 +27,8 @@
 					</li>
 				</ul>
 			</div>
+		</div>
+		<div id="wrapper">
 			<div id="optionsbar">
 				<ul class="options">
 					<li>
@@ -42,15 +44,62 @@
 					</li>
 				</ul>
 			</div>
-		</div>
-		<div id="wrapper">
-			<div id="main">
-				<div id="content">
+			
+			<div class="box_title">
+				<h1>WOOOOOOOOP!</h1>
+			</div>
+			<div class="box_main">
+				<div class="box_content">
+					<div class="content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed lectus lacus. Donec fringilla nisl lacus, sit amet mattis nulla fringilla quis. Quisque facilisis consequat sem suscipit porttitor. Nulla tortor erat, sollicitudin eget quam in, viverra hendrerit ipsum. Quisque volutpat lacinia risus, eget rhoncus urna fermentum dapibus. Sed congue elit metus, vitae hendrerit sapien scelerisque et. Donec et metus justo. Nam non elit blandit, eleifend sem non, interdum neque. Maecenas iaculis lorem lectus, id accumsan elit tempor ut. Nulla scelerisque, velit ac congue blandit, urna arcu aliquam mi, cursus euismod ligula erat et nulla. Aliquam erat volutpat. Praesent ultrices ornare erat, ac varius erat blandit ac. Pellentesque ut sapien at nisl iaculis iaculis. Suspendisse sagittis non turpis ut gravida. Fusce venenatis, elit in malesuada auctor, elit odio ultricies massa, eu congue neque nibh eget erat. Etiam ut ligula tempus, pharetra dolor vel, dapibus ante.
+						Donec et ornare est, et ornare felis. Duis venenatis tempus dignissim. Donec congue ante nec nisl varius suscipit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec non risus fringilla, congue nibh eu, auctor tellus. Donec imperdiet lobortis justo, non laoreet eros facilisis ut. Sed a imperdiet purus. Donec in neque in neque dapibus volutpat et non dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc lacinia ante sed tincidunt dapibus. Pellentesque rhoncus id neque id semper. Sed eget lorem ultricies metus ornare convallis. Integer gravida felis eget magna iaculis fermentum eget eu mi. Suspendisse porta mollis condimentum. Integer porta id nunc et sollicitudin.
+						Aliquam ac laoreet leo. Nam lectus tortor, dictum id viverra eu, fermentum vel nisl. Maecenas at ante ac nulla varius vestibulum sed a erat. Phasellus lorem massa, ultricies nec erat quis, dictum egestas diam. Nullam commodo condimentum enim id commodo. Donec sit amet venenatis lectus. Vivamus ultrices mi nec enim congue, fermentum convallis quam aliquet. Nulla quis quam interdum, imperdiet libero ut, egestas nisl. In a neque a metus dapibus semper id nec sapien. Nulla sodales a est ac mattis. Ut eu nisi non nisi lacinia vulputate eget sed quam.
+						Nam a nisl dapibus, consectetur lectus in, euismod nisl. Proin ornare risus non tortor facilisis, nec venenatis turpis aliquam. Sed risus dolor, faucibus ac ligula eget, auctor gravida lectus. Nunc ut ipsum libero. Nullam dignissim velit ante, et tempor lorem convallis eu. Nunc tincidunt velit eros. Aenean urna eros, elementum vel euismod vitae, blandit in risus. Donec enim felis, vulputate vel lobortis eget, gravida nec ante. Donec volutpat metus leo, sed tristique sem porttitor egestas. Pellentesque sagittis lacinia neque, vitae imperdiet nibh lobortis vitae. Nam porttitor aliquam lacus, vel commodo lectus hendrerit id. Praesent vestibulum pulvinar nisl, eu sollicitudin erat gravida at. Nunc turpis lectus, ultricies eu tristique sed, ullamcorper rutrum mi.
+						Pellentesque risus felis, rutrum ut dui ut, egestas rutrum ipsum. Aliquam porttitor odio ipsum, ut viverra mi tincidunt et. Donec non pretium eros. Donec mattis lorem non tempor pulvinar. Morbi lorem quam, vehicula et sodales in, pretium ut ligula. Morbi ultrices sollicitudin libero. Vestibulum tincidunt consequat enim, et fermentum orci sodales eget. Proin id ligula in massa pharetra egestas eu vel nisl. Nullam sed semper augue. 
+					</div>
 				</div>
-				<div id="bottom">
-					<span class="left">Copyright Philip Jensen</span>
-					<span class="right">page generated in: 237ms. sent 15 queries to the database.</span>
+			</div>
+			
+			<div class="box_title">
+				<h1>WOOOOOOOOP2!</h1>
+			</div>
+			<div class="box_main">
+				<div class="box_content_left">
+					<div class="content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed lectus lacus. Donec fringilla nisl lacus, sit amet mattis nulla fringilla quis. Quisque facilisis consequat sem suscipit porttitor. Nulla tortor erat, sollicitudin eget quam in, viverra hendrerit ipsum. Quisque volutpat lacinia risus, eget rhoncus urna fermentum dapibus. Sed congue elit metus, vitae hendrerit sapien scelerisque et. Donec et metus justo. Nam non elit blandit, eleifend sem non, interdum neque. Maecenas iaculis lorem lectus, id accumsan elit tempor ut. Nulla scelerisque, velit ac congue blandit, urna arcu aliquam mi, cursus euismod ligula erat et nulla. Aliquam erat volutpat. Praesent ultrices ornare erat, ac varius erat blandit ac. Pellentesque ut sapien at nisl iaculis iaculis. Suspendisse sagittis non turpis ut gravida. Fusce venenatis, elit in malesuada auctor, elit odio ultricies massa, eu congue neque nibh eget erat. Etiam ut ligula tempus, pharetra dolor vel, dapibus ante.
+						Donec et ornare est, et ornare felis. Duis venenatis tempus dignissim. Donec congue ante nec nisl varius suscipit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec non risus fringilla, congue nibh eu, auctor tellus. Donec imperdiet lobortis justo, non laoreet eros facilisis ut. Sed a imperdiet purus. Donec in neque in neque dapibus volutpat et non dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc lacinia ante sed tincidunt dapibus. Pellentesque rhoncus id neque id semper. Sed eget lorem ultricies metus ornare convallis. Integer gravida felis eget magna iaculis fermentum eget eu mi. Suspendisse porta mollis condimentum. Integer porta id nunc et sollicitudin.
+						Aliquam ac laoreet leo. Nam lectus tortor, dictum id viverra eu, fermentum vel nisl. Maecenas at ante ac nulla varius vestibulum sed a erat.
+					</div>
 				</div>
+				<div class="box_content_right">
+					<div class="content">
+						Phasellus lorem massa, ultricies nec erat quis, dictum egestas diam. Nullam commodo condimentum enim id commodo. Donec sit amet venenatis lectus. Vivamus ultrices mi nec enim congue, fermentum convallis quam aliquet. Nulla quis quam interdum, imperdiet libero ut, egestas nisl. In a neque a metus dapibus semper id nec sapien. Nulla sodales a est ac mattis. Ut eu nisi non nisi lacinia vulputate eget sed quam.
+						Nam a nisl dapibus, consectetur lectus in, euismod nisl. Proin ornare risus non tortor facilisis, nec venenatis turpis aliquam. Sed risus dolor, faucibus ac ligula eget, auctor gravida lectus. Nunc ut ipsum libero. Nullam dignissim velit ante, et tempor lorem convallis eu. Nunc tincidunt velit eros. Aenean urna eros, elementum vel euismod vitae, blandit in risus. Donec enim felis, vulputate vel lobortis eget, gravida nec ante. Donec volutpat metus leo, sed tristique sem porttitor egestas. Pellentesque sagittis lacinia neque, vitae imperdiet nibh lobortis vitae. Nam porttitor aliquam lacus, vel commodo lectus hendrerit id. Praesent vestibulum pulvinar nisl, eu sollicitudin erat gravida at. Nunc turpis lectus, ultricies eu tristique sed, ullamcorper rutrum mi.
+						Pellentesque risus felis, rutrum ut dui ut, egestas rutrum ipsum. Aliquam porttitor odio ipsum, ut viverra mi tincidunt et. Donec non pretium eros. Donec mattis lorem non tempor pulvinar. Morbi lorem quam, vehicula et sodales in, pretium ut ligula. Morbi ultrices sollicitudin libero. Vestibulum tincidunt consequat enim, et fermentum orci sodales eget. Proin id ligula in massa pharetra egestas eu vel nisl. Nullam sed semper augue. 
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<div class="box_title">
+				<h1>WOOOOOOOOP2!</h1>
+			</div>
+			<div class="box_main">
+				<div class="box_content_left">
+					<div class="content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed lectus lacus. Donec fringilla nisl lacus, sit amet mattis nulla fringilla quis. Quisque facilisis consequat sem suscipit porttitor. Nulla tortor erat, sollicitudin eget quam in, viverra hendrerit ipsum. Quisque volutpat lacinia risus, eget rhoncus urna fermentum dapibus. Sed congue elit metus, vitae hendrerit sapien scelerisque et. Donec et metus justo. Nam non elit blandit, eleifend sem non, interdum neque. Maecenas iaculis lorem lectus, id accumsan elit tempor ut. Nulla scelerisque, velit ac congue blandit, urna arcu aliquam mi, cursus euismod ligula erat et nulla. Aliquam erat volutpat. Praesent ultrices ornare erat, ac varius erat blandit ac. Pellentesque ut sapien at nisl iaculis iaculis. Suspendisse sagittis non turpis ut gravida. Fusce venenatis, elit in malesuada auctor, elit odio ultricies massa, eu congue neque nibh eget erat. Etiam ut ligula tempus, pharetra dolor vel, dapibus ante.
+						Donec et ornare est, et ornare felis. Duis venenatis tempus dignissim. Donec congue ante nec nisl varius suscipit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec non risus fringilla, congue nibh eu, auctor tellus. Donec imperdiet lobortis justo, non laoreet eros facilisis ut. Sed a imperdiet purus. Donec in neque in neque dapibus volutpat et non dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc lacinia ante sed tincidunt dapibus. Pellentesque rhoncus id neque id semper. Sed eget lorem ultricies metus ornare convallis. Integer gravida felis eget magna iaculis fermentum eget eu mi. Suspendisse porta mollis condimentum. Integer porta id nunc et sollicitudin.
+						Aliquam ac laoreet leo. Nam lectus tortor, dictum id viverra eu, fermentum vel nisl. Maecenas at ante ac nulla varius vestibulum sed a erat.
+					</div>
+				</div>
+				<div class="box_content_right">
+					<div class="content">
+						Phasellus lorem massa, ultricies nec erat quis, dictum egestas diam. Nullam commodo condimentum enim id commodo. Donec sit amet venenatis lectus. Vivamus ultrices mi nec enim congue, fermentum convallis quam aliquet. Nulla quis quam interdum, imperdiet libero ut, egestas nisl. In a neque a metus dapibus semper id nec sapien. Nulla sodales a est ac mattis. Ut eu nisi non nisi lacinia vulputate eget sed quam.
+						Nam a nisl dapibus, consectetur lectus in, euismod nisl. Proin ornare risus non tortor facilisis, nec venenatis turpis aliquam. Sed risus dolor, faucibus ac ligula eget, auctor gravida lectus. Nunc ut ipsum libero. Nullam dignissim velit ante, et tempor lorem convallis eu. Nunc tincidunt velit eros. Aenean urna eros, elementum vel euismod vitae, blandit in risus. Donec enim felis, vulputate vel lobortis eget, gravida nec ante. Donec volutpat metus leo, sed tristique sem porttitor egestas. Pellentesque sagittis lacinia neque, vitae imperdiet nibh lobortis vitae. Nam porttitor aliquam lacus, vel commodo lectus hendrerit id. Praesent vestibulum pulvinar nisl, eu sollicitudin erat gravida at. Nunc turpis lectus, ultricies eu tristique sed, ullamcorper rutrum mi.
+						Pellentesque risus felis, rutrum ut dui ut, egestas rutrum ipsum. Aliquam porttitor odio ipsum, ut viverra mi tincidunt et. Donec non pretium eros. Donec mattis lorem non tempor pulvinar. Morbi lorem quam, vehicula et sodales in, pretium ut ligula. Morbi ultrices sollicitudin libero. Vestibulum tincidunt consequat enim, et fermentum orci sodales eget. Proin id ligula in massa pharetra egestas eu vel nisl. Nullam sed semper augue. 
+					</div>
+				</div>
+				<div class="clear"></div>
 			</div>
 		</div>
 	</body>
