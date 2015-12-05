eec7626ecc055ed62a18371af3393c59e90f794f
Philip Jensen <me@iensenfirppu.dk>
2015-12-05 18:20:01 +0100

    Fixed Travis integration #4

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -10,11 +10,11 @@ class FunctionsTest extends PHPUnit_Framework_TestCase
     
     /*public function testConvertTextToHtml()
     {
-		$text = 'This is a line of text.\n\tHere is another line, but it starts with a TAB!\n&qoute;This &lt;line&gt; has special characters in it&qoute;&trade;';
+		$text = 'This is a line of text.\n\tHere is another line, but it starts with a TAB!\n&qoute;This &lt;line&gt; has special characters in it&qoute;';
 		
 		$html = "This is a line of text.
 	Here is another line, but it starts with a TAB!
-\"This <line> has special characters in it\"™";
+\"This <line> has special characters in it\"";
 		
 		echo $text;
 		echo $html;
@@ -24,11 +24,11 @@ class FunctionsTest extends PHPUnit_Framework_TestCase
     
     public function testConvertHtmlToText()
     {
-		$text = 'This is a line of text.\n\tHere is another line, but it starts with a TAB!\n&qoute;This &lt;line&gt; has special characters in it&qoute;&trade;';
+		$text = 'This is a line of text.\n\tHere is another line, but it starts with a TAB!\n&qoute;This &lt;line&gt; has special characters in it&qoute;';
 		
 		$html = "This is a line of text.
 	Here is another line, but it starts with a TAB!
-\"This <line> has special characters in it\"™";
+\"This <line> has special characters in it\"";
 		
 		$this->assertEquals($text, reiZ::ConvertHtmlToText($html));
     }*/
